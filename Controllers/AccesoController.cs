11d5c9dffff72dad451ee7baab1b7c6493877339
Kevin Barrantes Montoya <kevin.barrantes.montoya@cuc.cr>
2023-07-15 22:52:00 -0600

    Add project files.

---
+++ Controllers/AccesoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BabyGuide.Controllers
+{
+    public class AccesoController : Controller
+    {
+        // GET: Acceso
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
