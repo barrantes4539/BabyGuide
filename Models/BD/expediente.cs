fd9d7bd776fc2f6a54de95c46c7ca55b9d1a8413
ElJason <jassonquiros753@gmail.com>
2023-07-24 20:03:59 -0600

    Conexion a BD

---
+++ Models/BD/expediente.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BabyGuide.Models.BD
+{
+    public class expediente
+    {
+        public DataTable VerAlergias()
+        {
+            SqlConnection connection = new SqlConnection();
+            try
+            {
+                string connectionString = Conexion.cn;
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+                SqlCommand command = new SqlCommand("spVerAlergias", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                //command.Parameters.AddWithValue("@ced", ced);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                return dataTable;
+            }
+            catch (Exception)
+            {
+                return new DataTable();
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+    }
+    
+}
