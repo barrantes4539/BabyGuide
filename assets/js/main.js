2ea7158c318114b84489bef35cdd65d227ffc346
Kevin Barrantes Montoya <kevin.barrantes.montoya@cuc.cr>
2023-07-31 15:10:01 -0600

    Consejos index

--- assets/js/main.js
+++ assets/js/main.js
@@ -159,6 +159,42 @@
     }
   });
 
+    /**
+   * Testimonials Slider
+   */
+    new Swiper('.testimonials-slider', {
+        speed: 600,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        slidesPerView: 'auto',
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        }
+    });
+
+    /**
+     * Testimonials Slider
+     */
+    new Swiper('.portfolio-details-slider', {
+        speed: 600,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        slidesPerView: 'auto',
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        }
+    });
+
   /**
    * Porfolio isotope and filter
    */
@@ -186,6 +222,7 @@
     }
 
   });
+
 
 
 })()
