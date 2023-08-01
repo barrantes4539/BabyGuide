06ae139a75c5f041afc6e7493b186c791d98df3d
Kevin Barrantes Montoya <kevin.barrantes.montoya@cuc.cr>
2023-08-01 00:12:20 -0600

    Autenticacion realizada

---
+++ Models/Listas/UsuariosModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BabyGuide.Models.Listas
+{
+    public class UsuariosModel
+    {
+        public string NombreUsuario { get; set; }
+        public string Apellido1 { get; set; }
+        public string Apellido2 { get; set; }
+        public string CorreoE { get; set; }
+        public string Clave { get; set; }
+
+    }
+}
